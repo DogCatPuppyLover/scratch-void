127b90386d80589af16cdc4070c94fa25b9fe216
Isla <79656307+DogCatPuppyLover@users.noreply.github.com>
2022-07-04 17:12:19 -0700

    Update void.py

--- void.py
+++ void.py
@@ -25,62 +25,80 @@ with requests.Session() as s:
 
     # Login with user info
     r = s.post("https://scratch.mit.edu/login/", data=data, headers=headers)
-    print(r.status_code)
-    session_id = re.search('"(.*)"', r.headers["Set-Cookie"]).group()
-    token = r.json()[0]["token"]
+    if r.status_code == 200:
+        print(r.status_code)
+        session_id = re.search('"(.*)"', r.headers["Set-Cookie"]).group()
+        token = r.json()[0]["token"]
 
-    # Set headers
-    headers = {
+        # Set headers
+        headers = {
+                "x-requested-with": "XMLHttpRequest",
+                "Cookie": "scratchlanguage=en;permissions=%7B%7D;",
+                "referer": "https://scratch.mit.edu"
+        }
+
+        # Get CSRF token
+        r = s.get("https://scratch.mit.edu/csrf_token/", headers=headers)
+        print(r.status_code)
+        csrf_token = re.search(
+                "scratchcsrftoken=(.*?);", r.headers["Set-Cookie"]
+        ).group(1)
+
+        # Update headers with the CSRF token, token, and cookies
+
+        headers = {
+            "x-csrftoken": csrf_token,
+            "X-Token": token,
             "x-requested-with": "XMLHttpRequest",
-            "Cookie": "scratchlanguage=en;permissions=%7B%7D;",
-            "referer": "https://scratch.mit.edu"
-    }
+            "Cookie": "scratchcsrftoken="
+            + csrf_token
+            + ";scratchlanguage=en;scratchsessionsid="
+            + session_id
+            + ";",
+            "referer": "https://scratch.mit.edu",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36"
+        }
 
-    # Get CSRF token
-    r = s.get("https://scratch.mit.edu/csrf_token/", headers=headers)
-    print(r.status_code)
-    csrf_token = re.search(
-            "scratchcsrftoken=(.*?);", r.headers["Set-Cookie"]
-    ).group(1)
+        print(csrf_token)
 
-    # Update headers with the CSRF token, token, and cookies
-
-    headers = {
-        "x-csrftoken": csrf_token,
-        "X-Token": token,
-        "x-requested-with": "XMLHttpRequest",
-        "Cookie": "scratchcsrftoken="
-        + csrf_token
-        + ";scratchlanguage=en;scratchsessionsid="
-        + session_id
-        + ";",
-        "referer": "https://scratch.mit.edu",
-    }
-
-    print(csrf_token)
-
-    # Get session data
-    session = s.get("https://scratch.mit.edu/session/", headers=headers)
-    print(session.status_code)
-    print(session.text)
-    
-    wait = 120
-    
-    while True:
-        html = s.get("https://scratch.mit.edu/site-api/comments/user/" + username + "/?page=1").text
-        soup = BeautifulSoup(html, "html.parser")
-        comments = soup.find_all("div", class_="comment")
-        if (len(comments) == 0):
-            wait = wait * 2
-        for i in comments:
-            data = json.dumps({"id":i["data-comment-id"]})
-            s.post("https://scratch.mit.edu/site-api/comments/user/" + username + "/del/", data=data, headers=headers)
-            print("#" + i["data-comment-id"])
-            print("@" + i.find(class_="name").text.strip() + ":")
-            print(" ".join(i.find(class_="content").text.split()))
-            print("")
-            wait = wait / 2
+        # Get session data
+        session = s.get("https://scratch.mit.edu/session/", headers=headers)
+        print(session.status_code)
+        print(session.text)
         
-        wait = max(3.75, min(wait, 120))
+        r = s.post("https://scratch.mit.edu/site-api/comments/user/__V01D__/add/", headers=headers, data=json.dumps({"content": "void", "parent_id": "", "commentee_id": ""}))
+        print(r.status_code)
         
-        time.sleep(wait)
+        if r.text == "<script id=\"error-data\" type=\"application/json\">{\"error\": \"isDisallowed\"}</script>":
+            print("Comments enabled.")
+            s.post("https://scratch.mit.edu/site-api/comments/user/" + username + "/toggle-comments/", headers=headers)
+        
+        wait = 120
+        
+        try:
+            while True:
+                html = s.get("https://scratch.mit.edu/site-api/comments/user/" + username + "/?page=1").text
+                soup = BeautifulSoup(html, "html.parser")
+                comments = soup.find_all("div", class_="comment")
+                if (len(comments) == 0):
+                    wait = wait * 2
+                for i in comments:
+                    r = s.post("https://scratch.mit.edu/site-api/comments/user/" + username + "/del/", data=json.dumps({"id":i["data-comment-id"]}), headers=headers)
+                    print(r.status_code)
+                    print("#" + i["data-comment-id"])
+                    print("@" + i.find(class_="name").text.strip() + ":")
+                    print(" ".join(i.find(class_="content").text.split()))
+                    print("")
+                    wait = wait / 2
+                
+                wait = max(3.75, min(wait, 120))
+                
+                time.sleep(wait)
+            
+        except:
+            s.post("https://scratch.mit.edu/site-api/comments/user/" + username + "/toggle-comments/", headers=headers)
+            print("Comments closed.")
+
+    else:
+        print("Login failed")
+        print(r.status_code)
